7387e878bd34a1c70e83ed1ff4252dc522303727
Masood Dalman <m.dalman68@gmail.com>
2022-06-14 16:24:31 +0300

    add setting menu in main activity

--- app/src/main/java/com/liliputdev/mvvmexample/ui/mainActivity/MainActivity.kt
+++ app/src/main/java/com/liliputdev/mvvmexample/ui/mainActivity/MainActivity.kt
@@ -2,11 +2,17 @@ package com.liliputdev.mvvmexample.ui.mainActivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.liliputdev.mvvmexample.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
