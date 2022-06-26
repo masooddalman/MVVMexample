c58e0ad92a6be6d4dc55bdc6d28c87651401c780
Masood Dalman <m.dalman68@gmail.com>
2022-06-26 12:38:32 +0300

    make WebRepository to use retrofit interface

---
+++ app/src/main/java/com/liliputdev/mvvmexample/repository/retrofit/WebRepository.kt
@@ -0,0 +1,8 @@
+package com.liliputdev.mvvmexample.repository.retrofit
+
+/**
+ * Created by Masood Dalman on 6/26/2022.
+ */
+class WebRepository constructor(private val service: RetrofitService) {
+    
+}
