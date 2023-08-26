9e5426e48f4a34f74c8b1ffaf7c0d1ae5ffe73e2
masooddalman <m.dalman68@gmail.com>
2023-08-26 14:31:02 +0300

    favorite is added to recycler adapter

--- app/src/main/java/com/liliputdev/mvvmexample/adapters/ProductListAdapter.kt
+++ app/src/main/java/com/liliputdev/mvvmexample/adapters/ProductListAdapter.kt
@@ -2,9 +2,12 @@ package com.liliputdev.mvvmexample.adapters
 
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
+import android.content.res.ColorStateList
 import android.view.View
 import android.view.animation.OvershootInterpolator
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import androidx.core.widget.ImageViewCompat
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 import com.liliputdev.mvvmexample.R
@@ -17,13 +20,35 @@ import com.squareup.picasso.Picasso
 class ProductListAdapter:BaseQuickAdapter<APIModelAllProductElement, BaseViewHolder>(R.layout.item_product) {
     override fun convert(holder: BaseViewHolder, item: APIModelAllProductElement) {
 
-       holder.setText(R.id.itemTextViewProductTitle,item.title).
+       holder.setText(R.id.itemTextViewProductTitle, item.title).
                setText(R.id.itemTextViewProductPrice,"$${item.price}").
                setText(R.id.itemTextViewProductRate,"${item.rating.rate} / ${item.rating.count}")
 
         Picasso.get().load(item.image).centerCrop().resize(300,300).into(holder.getView(R.id.itemImageViewProductImage) as ImageView)
-
+        holder.getView<ImageView>(R.id.itemImageviewFav).let {
+            if (item.isfaved)
+                ImageViewCompat.setImageTintList(
+                    it,
+                    ColorStateList.valueOf(
+                        ContextCompat.getColor(
+                            context,
+                            R.color.design_default_color_primary
+                        )
+                    )
+                )
+            else
+                ImageViewCompat.setImageTintList(
+                    it,
+                    ColorStateList.valueOf(
+                        ContextCompat.getColor(
+                            context,
+                            R.color.md_theme_dark_primary
+                        )
+                    )
+                )
+        }
         animateItem(holder.itemView,1000)
+        addChildClickViewIds(R.id.itemImageviewFav)
     }
 
     private fun animateItem(itemView: View, duration:Long)
